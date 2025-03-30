225c3026d79cf313f3aa07cee02b2d1535f775c0
jaervin1 <jaervin@email.sc.edu>
2025-03-30 12:42:40 -0400

    Various changes to home page

--- outdoor-sc/src/pages/Home.jsx
+++ outdoor-sc/src/pages/Home.jsx
@@ -3,15 +3,23 @@ import Tree from "../components/Tree";
 function Home() {
   return (
     <>
-      <section className="columns">
-        <Tree 
-        name="Live Oak"
-        description="Doesn't lose it's leaves"
-        image="images/live_oak.jpeg"/>
-        <Tree
-        name="Dogwood"
-        description="Flower's in spring" 
-        image="images/dogwood.jpeg"/>
+      <section class="featured content">
+        <h2>Featured Activity</h2>
+        <img src="images/boardwalk-trail.jpg" />
+        <a href="activity.html"><h2>Boardwalk Loop</h2></a>
+      </section>
+      <section className="recommended content">
+        <h2>For You</h2>
+        <div className="columns">
+            <section>
+                <img src="images/trail-one.jpg"/>
+                <a href=""><h2>Peak to Pemaria</h2></a>
+            </section>
+            <section>
+                <img src="images/trail-two.jpg"/>
+                <a href=""><h2>Hellwood Loop</h2></a>
+            </section>
+        </div>
       </section>
     </>
   );
