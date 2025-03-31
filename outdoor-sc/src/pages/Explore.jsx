b523ad6669f6488114f208594c7c6da735b9fd56
jaervin1 <jaervin@email.sc.edu>
2025-03-31 00:22:45 -0400

    lots of changes. including add_activity_modal based on what I found on geeks for geeks

--- outdoor-sc/src/pages/Explore.jsx
+++ outdoor-sc/src/pages/Explore.jsx
@@ -1,13 +1,22 @@
 import "./css/Explore.css";
-import {FaPlusCircle, FaMapMarker} from "react-icons/fa";
+import { FaPlusCircle, FaMapMarker, FaArrowLeft } from "react-icons/fa";
+import { useState } from "react";
+
+
 
 function Explore() {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
     return (
         <main id="explore-page">
             <section class="search-container">
                 <input type="text" class="search-input" placeholder="Search" />
                 <input type="text" class="zipcode-input" placeholder="29201" />
-                <FaMapMarker color="#344e41"/>
+                <FaMapMarker color="#344e41" />
             </section>
 
             <section class="filters">
@@ -15,10 +24,37 @@ function Explore() {
                 <button>Rating (stars)</button>
                 <button>Difficulty</button>
                 <button>Activity Type</button>
-                <a href="add_activity.html"
-                ><button> <FaPlusCircle/> </button>
-                </a>
+                <button onClick={openModal}> <FaPlusCircle /> Add Activity </button>
             </section>
+
+            {isModalOpen && (
+                <div id="myModal" className="modal">
+                    <div className="modal-content">
+                        <button className="close" onClick={closeModal}><FaArrowLeft/></button>
+                        <h2>Add Activity</h2>
+                        <hr />
+                        <div className="activity-field">
+                            <h3>Activity Name:</h3>
+                            <input type="text" className="user-input" />
+                        </div>
+                        <div className="activity-field">
+                            <h3>Description:</h3>
+                            <input type="text" className="user-input" />
+                        </div>
+                        <div className="activity-field">
+                            <h3>Location:</h3>
+                            <input type="text" className="user-input" />
+                        </div>
+                        <div className="activity-field">
+                            <h3>Images:</h3>
+                            <input type="file" id="photo-browse" />
+                        </div>
+                        <div id="submit-field">
+                            <button id="submit-button">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </main>
     );
 };
